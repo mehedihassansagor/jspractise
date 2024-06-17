635782235a736c2f61d53cc27b949e5599db1919
mehedihassansagor <mehedihassansagor01@gmail.com>
2024-06-17 15:04:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+//switch case 
+// var marks = prompt("please enter the marks: ")
+// var grade = marks/10;
+
+// switch(grade){
+//     case 10 : 
+//     document.write("<h1> marks is A+ </h1>");
+//     break;
+
+//     case 9 : 
+//     document.write("<h1> marks is A </h1>");
+//     break;
+
+//     case 8 : 
+//     document.write("<h1> marks is A- </h1>");
+//     break;
+
+//     case 7 : 
+//     document.write("<h1> marks is B+ </h1>");
+//     break;
+
+    
+// default: 
+// document.write("<h1> TMRA SOBAY MOFIZ </h1>");
+
+   
+// }
+
+
+
+//continue break
+// var value = prompt("enter the number: ")
+// if(index == 5){
+//     document.write(index)
+//     document.write("<h1> ami r parmu na </h1>")
+//     console.log(index)
+//     break;
+// }
+
+// console.log(value)
+for (let index = 0; index < 10; index++) {
+    if(index === 5){
+        continue;
+    }
+    console.log(index)  
+
+}
